768cdf6da249ae936b22a996a6bd00bfd37b9471
Yifan Gao <git@yfgao.com>
2017-01-24 00:58:47 +0600

    add deploy script

---
+++ upload.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/gaoyifan/china-operator-ip.git"
+git add result
+git commit -m "update `date +%Y-%m-%d`"
+git push origin ${TRAVIS_BRANCH}
