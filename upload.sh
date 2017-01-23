d704399010af092142585ce7846c4767426fde9d
Yifan Gao <git@yfgao.com>
2017-01-24 02:50:53 +0600

    upload.sh: fix push failed

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@
 git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/gaoyifan/china-operator-ip.git"
 git add result
 git commit -m "update `date +%Y-%m-%d`"
-git push origin ${TRAVIS_BRANCH}
+git push origin $(git rev-parse HEAD):${TRAVIS_BRANCH}
