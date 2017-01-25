17d9e42d5d36c4d1a2577d709b5b994dc61b1f64
Yifan Gao <git@gaoyifan.com>
2017-01-26 02:57:58 +0800

    separate stat procedure

--- generate.sh
+++ generate.sh
@@ -41,21 +41,3 @@ while read file; do
 	get_asn $file | xargs bgptools | docker run -i --rm gaoyifan/cidrmerge > result/$operator.txt
 	>&2 echo "done"
 done
-
-# count the number of IP
-cd result
-ls *.txt | 
-while read file; do 
-	echo ${file%.*}
-	cat $file | 
-	awk -F\/ '{print $2}' | 
-	(
-		sum=0
-		while read n; do
-			((s=32-n))
-			((sum+=1<<s))
-		done
-		echo $sum
-	)
-	echo
-done > stat
---
+++ stat.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd result
+ls *.txt | 
+while read file; do 
+	echo ${file%.*}
+	cat $file | 
+	awk -F\/ '{print $2}' | 
+	(
+		sum=0
+		while read n; do
+			((s=32-n))
+			((sum+=1<<s))
+		done
+		echo $sum
+	)
+	echo
+done > stat
