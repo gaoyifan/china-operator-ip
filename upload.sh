d6c3f9dfacf5deee5f871d3a518ec2c15e6173a3
Yifan Gao <git@yfgao.com>
2017-03-08 01:34:20 +0800

    fix CI commit with wrong author

--- upload.sh
+++ upload.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-git config user.name $GIT_USER_NAME
-git config user.email $GIT_USER_EMAIL
-
 git clone -b ip-lists https://${GH_REF} ip-lists
 mv result/* ip-lists
 cd ip-lists
+git config user.name $GIT_USER_NAME
+git config user.email $GIT_USER_EMAIL
 git add .
 git commit -m "update $(date +%Y-%m-%d)"
 git push -q "https://${GH_TOKEN}@${GH_REF}" ip-lists:ip-lists
