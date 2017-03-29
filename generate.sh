e4a73612f1b99b667059b96c9d424b749fec333b
Yifan Gao <git@yfgao.com>
2017-03-29 17:00:53 +0800

    update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,8 +4,7 @@ source common.sh
 set -e
 [[ $SKIP_DATA_PREPARATION != true ]] && prepare_data
 mkdir -p result
-ls operator/*.conf |
-while read file; do
+for file in operator/*.conf; do
 	operator=${file%.*}
 	operator=${operator##*/}
 	log_info "generating IP list of $operator ..."
