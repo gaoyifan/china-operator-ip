da4035772720bdb9c9e864b3e0734c0c35c59dd1
Yifan Gao <git@yfgao.com>
2019-12-19 15:58:59 +0800

    Merge operator6/ into operator/

--- generate.sh
+++ generate.sh
@@ -8,14 +8,8 @@ for file in operator/*.conf; do
 	operator=${file%.*}
 	operator=${operator##*/}
 	log_info "generating IP list of $operator ..."
-	get_asn $file | tee /dev/stderr | xargs bgptools -b rib.txt | sort | uniq | docker run -i --rm yangzhaofengsteven/cidr-merge > result/$operator.txt
-	log_info "done"
+	get_asn $file
+	get_asn $file | xargs bgptools -b rib.txt  | sort | uniq | docker run -i --rm yangzhaofengsteven/cidr-merge > result/${operator}.txt
+	get_asn $file | xargs bgptools -b rib6.txt | sort | uniq | docker run -i --rm yangzhaofengsteven/cidr-merge > result/${operator}6.txt
 done
 
-for file in operator6/*.conf; do
-	operator=${file%.*}
-	operator=${operator##*/}
-	log_info "generating IPv6 list of $operator ..."
-	get_asn $file | tee /dev/stderr | xargs bgptools -b rib6.txt | sort | uniq | docker run -i --rm yangzhaofengsteven/cidr-merge > result/${operator}6.txt
-	log_info "done"
-done
