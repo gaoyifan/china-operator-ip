eeae995e4ee4cd3e174fa0dbb6fd380f12d7fce7
Yifan Gao <git@yfgao.com>
2019-12-19 14:47:56 +0800

    Refactoring stat.sh

--- stat.sh
+++ stat.sh
@@ -2,34 +2,25 @@
 
 source common.sh
 cd result
-for file in *.txt; do 
+for file in *.txt; do
 	echo ${file%.*}
 	if [[ $file == *6.txt ]]; then
-		cat $file |
-		awk -F\/ '{print $2}' |
-		(
-			sum=0
-			while read n; do
-				if [[ -n $n ]]; then
-					((s=64-n))
-					((sum+=1<<s))
-				fi
-			done
-			echo $sum
-			)
-	else
-		cat $file | 
-		awk -F\/ '{print $2}' | 
-		(
-			sum=0
-			while read n; do
-				if [[ -n $n ]]; then
-					((s=32-n))
-					((sum+=1<<s))
-				fi
-			done
-			echo $sum
-		)
-	fi
+        #statistics IPv6 /48 blocks
+        base=48
+    else
+        base=32
+    fi
+	cat $file |
+	awk -F\/ '{print $2}' |
+	(
+		sum=0
+		while read n; do
+			if [[ -n $n ]] && [[ $n -le $base ]]; then
+				((s=base-n))
+				((sum+=1<<s))
+			fi
+		done
+		echo $sum
+	)
 	echo
 done | tee stat
