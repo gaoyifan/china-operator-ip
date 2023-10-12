2f87fb22ddcfb62fc4410af848dba3f8ccfda13b
Zhaofeng Yang <yangzhaofeng@arcas-da.com>
2023-10-12 14:47:54 +0800

    generate latest-bview.txt to /mnt

--- common.sh
+++ common.sh
@@ -9,9 +9,9 @@ log_info(){
 prepare_data(){
 	curl -sSLo latest-bview.gz http://data.ris.ripe.net/rrc00/latest-bview.gz
 	log_info "runing bgpdump ..."
-	bgpdump -m -O latest-bview.txt latest-bview.gz
-	grep -v '::/' < latest-bview.txt > rib.txt & # prepare_data_v4
-	grep '::/' < latest-bview.txt > rib6.txt & # prepare_data_v6
+	bgpdump -m -O ${BVIEW_PATH}latest-bview.txt latest-bview.gz
+	grep -v '::/' < ${BVIEW_PATH}latest-bview.txt > rib.txt & # prepare_data_v4
+	grep '::/' < ${BVIEW_PATH}latest-bview.txt > rib6.txt & # prepare_data_v6
 	wait_exit
 }
 
