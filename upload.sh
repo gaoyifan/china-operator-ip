8186678bfab826f1bf1ae22d680518fbf793a0f5
iBug <git@ibugone.com>
2025-08-08 00:19:40 +0800

    Use identity for @web-flow in upload.sh

--- upload.sh
+++ upload.sh
@@ -4,9 +4,8 @@ rm ip-lists/*.txt
 mv result/* ip-lists
 cd ip-lists
 tree -H . -P "*.txt|stat" -T "China Operator IP - prebuild results" > index.html
-git config user.name GitHub Actions
-git config user.email github-actions@github.com
+git config user.name "GitHub Actions"
+git config user.email noreply@github.com
 git add .
 git commit -m "update $(date +%Y-%m-%d)"
 git push -q
-
